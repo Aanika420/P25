b06f1d3a46b5cb1894c24780528d853ed640e01e
Aanika420 <76187900+Aanika420@users.noreply.github.com>
2021-03-04 17:24:16 -0500

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,22 @@
+class paper
+{
+    constructor(x,y){
+        var options ={
+          isStatic: false,
+          restitution:0.3,
+          friction:0.5,
+          density:1.2
+        };
+        this.body = Bodies.circle(x,y,10);
+        this.image = loadImage("paper.png");
+        World.add(world,this.body);
+    }
+    display(){
+        push();
+        if(keyDown(upArrowKey)){
+            this.body.velocityY = -3;
+        }
+        fill('white');
+        pop();
+    }
+}
